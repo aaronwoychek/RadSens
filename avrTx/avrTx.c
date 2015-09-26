cde1122ac3cf14d02db174b07a637464d62fbd89
Sawaiz Syed <github@sawaizsyed.com>
2015-09-25 23:09:56 -0400

    New Makefile, main does not get stuck

--- avrTx/avrTx.c
+++ avrTx/avrTx.c
@@ -1,25 +1,21 @@
-/*
- * avrTx.c
- *
- * Created: 07/17/14 23:03:39
- *  Author: Sawaiz, aaron
- */
-
-#define F_CPU 1000000UL
-#define deviceID 0x17
+//avrTx.c
+//Sawaiz, Aaron
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "nrf24/nrf24.h"
 
+#define deviceID 0x10
+#define ledPin PIND5
+#define ledPort DDRD
+
 uint16_t events = 0;
 uint8_t data_array[4];
 uint8_t tx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
 uint8_t rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
 
-ISR(INT0_vect)
-{
+ISR(INT0_vect) {
  cli();
  PORTB ^= 1<<PORTB3;
  while (!(PIND & 1<<PIND2)){
@@ -28,30 +24,29 @@ ISR(INT0_vect)
  sei();
 }
 
-void pwm_init(){
- // initialize TCCR0 as per requirement, say as follows
- TCCR0A |= (1<<WGM00)|(1<<COM0A1)|(1<<WGM01);
+void pwm_init() {
+ TCCR0A |= (1<<WGM00);
+ TCCR0A |= (1<<WGM01);
+ TCCR0A |= (1<<COM0A1);
  TCCR0B |= (1<<CS00);
- TCCR0B |= (0<<CS01);
- TCCR0B |= (0<<CS02);
-
  // make sure to make OC0 as output pin
  DDRB |= (1<<PORTB2);
-
- OCR0A = 127; //Set to 400V
+ OCR0A = 127;          //Set to 400V
 }
 
-void interupt_init(){
- GIMSK = 1 << INT0;   // Enable INT0
+void interupt_init() {
+ GIMSK = 1 << INT0;    // Enable INT0
  MCUCR |= 0 << ISC00;  // Trigger INT0 on low level
  MCUCR |= 0 << ISC01;
  DDRD |= 0 << PORTD2;  //Make Sure its an input as well.
- sei();    //enable global interrupts
+ sei();                //enable global interrupts
 }
 
-int main(){
+int main() {
  DDRB |= 1<<PORTB3;
  PORTB |= 1<<PORTB3;
+
+ ledPort |= _BV(ledPin);
 
  /* init hardware pins */
  nrf24_init();
@@ -70,18 +65,16 @@ int main(){
  data_array[0] = deviceID;
  data_array[1] = 0xAA;
 
- DDRA |= _BV(PA1);  //This should technially be DDA1
-
- while(1){
+ while(1) {
   data_array[3] = events;
   data_array[2] = events >> 8;
   nrf24_send(data_array);
 
-//   /* Wait for transmission to end */
-   while(nrf24_isSending());
+  /* Wait for transmission to end */
+  while(nrf24_isSending()) {
+  }
 
-  PORTA ^= _BV(PA1);
-
+  ledPort ^= _BV(ledPin);
   _delay_ms(1000);
  }
 }
